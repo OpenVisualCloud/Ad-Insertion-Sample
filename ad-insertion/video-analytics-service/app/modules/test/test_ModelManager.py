dbd3f030d796d2da7352e490a531b9e535912786
Cody Douglas <cody.g.douglas@intel.com>
2019-04-12 13:15:25 -0700

    Negative destination and fixed FFmpeg tests
    Fix tests and PipelineManager.py so they use cross-platform os.path methods

--- ad-insertion/video-analytics-service/app/modules/test/test_ModelManager.py
+++ ad-insertion/video-analytics-service/app/modules/test/test_ModelManager.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.append(os.path.dirname(__file__) + "/../../")
+sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
 import unittest
 import common
