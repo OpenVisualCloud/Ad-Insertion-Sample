1d91f97f255d013c82c733889cac41d42bf5ca45
nnshah1 <neelay.n.shah@intel.com>
2019-07-11 14:49:50 -0700

    updating update.sh to latest

--- ad-insertion/video-analytics-service/update.sh
+++ ad-insertion/video-analytics-service/update.sh
@@ -1,21 +1,42 @@
 #!/bin/bash -e
 
 DIR=$(dirname $(readlink -f "$0"))
+XEON_DIR=$DIR/platforms/Xeon
+VCAC_A_DIR=$DIR/platforms/VCAC-A
 
 # ffmpeg base dockerfile
-DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.0/Xeon/ubuntu-18.04/dldt%2Bffmpeg"
+DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.2/Xeon/ubuntu-18.04/analytics/ffmpeg"
 
 base_name=xeon-ubuntu1804-dldt-ffmpeg-va
-docker_file="$DIR/Dockerfile.4.ffmpeg"
+docker_file="$XEON_DIR/Dockerfile.4.ffmpeg"
 
 echo "# "${base_name} > ${docker_file}
 curl ${DOCKER_REPO}/Dockerfile >> ${docker_file}
 
 # gst base dockerfile
-DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.0/Xeon/ubuntu-18.04/dldt%2Bgst"
+DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.2/Xeon/ubuntu-18.04/analytics/gst"
 
 base_name=xeon-ubuntu1804-dldt-gst-va
-docker_file="$DIR/Dockerfile.2.gst"
+docker_file="$XEON_DIR/Dockerfile.2.gst"
 
 echo "# "${base_name} > ${docker_file}
 curl ${DOCKER_REPO}/Dockerfile>> ${docker_file}
+
+# gst base dockerfile vcac-a
+DOCKER_REPO="https://raw.githubusercontent.com/fkhoshne/Dockerfiles/vcaca/VCAC-A/ubuntu-16.04/analytics/gst"
+
+base_name=vcac-a-ubuntu1604-openvino-gst-va
+docker_file="$VCAC_A_DIR/Dockerfile.2.gst.vcac-a"
+
+echo "# "${base_name} > ${docker_file}
+curl ${DOCKER_REPO}/Dockerfile>> ${docker_file}
+
+# ffmpeg base dockerfile vcaa
+DOCKER_REPO="https://raw.githubusercontent.com/fkhoshne/Dockerfiles/vcaca/VCAC-A/ubuntu-16.04/analytics/ffmpeg"
+
+base_name=vcac-a-ubuntu1604-openvino-ffmpeg-va
+docker_file="$VCAC_A_DIR/Dockerfile.4.ffmpeg.vcac-a"
+
+echo "# "${base_name} > ${docker_file}
+curl ${DOCKER_REPO}/Dockerfile >> ${docker_file}
+
