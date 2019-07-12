e3af3e3d3928ea78c7ddd5549d7ca64ce9cef9f1
nnshah1 <neelay.n.shah@intel.com>
2019-07-11 20:09:17 -0700

    incremental progress

--- ad-insertion/video-analytics-service/update.sh
+++ ad-insertion/video-analytics-service/update.sh
@@ -8,7 +8,7 @@ VCAC_A_DIR=$DIR/platforms/VCAC-A
 DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.2/Xeon/ubuntu-18.04/analytics/ffmpeg"
 
 base_name=xeon-ubuntu1804-dldt-ffmpeg-va
-docker_file="$XEON_DIR/Dockerfile.4.ffmpeg"
+docker_file="$XEON_DIR/Dockerfile.4.ffmpeg.xeon"
 
 echo "# "${base_name} > ${docker_file}
 curl ${DOCKER_REPO}/Dockerfile >> ${docker_file}
@@ -17,7 +17,7 @@ curl ${DOCKER_REPO}/Dockerfile >> ${docker_file}
 DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v1.2/Xeon/ubuntu-18.04/analytics/gst"
 
 base_name=xeon-ubuntu1804-dldt-gst-va
-docker_file="$XEON_DIR/Dockerfile.2.gst"
+docker_file="$XEON_DIR/Dockerfile.2.gst.xeon"
 
 echo "# "${base_name} > ${docker_file}
 curl ${DOCKER_REPO}/Dockerfile>> ${docker_file}
