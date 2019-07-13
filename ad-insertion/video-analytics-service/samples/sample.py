2ac2f3caf8623245020d0fc00a76cc9c1998f8c9
nnshah1 <neelay.n.shah@intel.com>
2019-07-12 22:53:49 -0700

    Converged (#39)
    * initial changes for recording
    
    * small fixes
    
    * Added the creation of folder structure to recordings based on year month
    date
    
    * Moved code to fix issue with creating proper directory path
    
    * Updated Dockerfile for latest version of gst and gst plugins
    
    * Updated VA Service Dockerfiles for va plugin fix and added metapublish
    to pipeline
    
    * Added sensor id to recording path
    
    * changed properties to parameters
    
    * Set recording output directory based on request parameter recording_prefix
    
    * pipeline latency
    
    * latency not in status if invalid
    
    * Testing metapublish refactor
    
    * Updated for refactor for metapublish
    
    * Updated metapublish
    
    * Prepared for external release
    
    * Fixed potential memory issue
    
    * Added kafka and file support
    
    * simplfying recording name
    
    * adding fp16 and int8 models and refactor modelmanager to read model structure dynamically
    
    * updating pipelines to match latest
    
    * updating update.sh to latest
    
    * Updated Dockerfile with file and kafka support
    
    * incremental progress
    
    * updating gstreamer va plugins for consistency
    
    * fixing naming for record and latency
    
    * fixes from merge and support for parsing fmpeg file results
    
    * fixing typo
    
    * renaming for consistency
    
    * fixed special targets
    
    * fixing typo
    
    * fixing vcac-a configurations
    
    * code review fixes

--- ad-insertion/video-analytics-service/samples/sample.py
+++ ad-insertion/video-analytics-service/samples/sample.py
@@ -44,22 +44,40 @@ def print_json(object):
                      separators=[',',': ']))
     
 def read_detection_results(destination,verbose=True):
-    if (verbose):
+    if verbose:
+        object_lines=[]
         with open(destination) as file:
             for line in file:
-                print("Detection Result: \n")
-                print_json(json.loads(line))
-            
+                try:
+                    if line=="{\n":
+                        object_lines.append(line)
+                        line=None
+                        
+                    if object_lines:
+                        if line:
+                            object_lines.append(line)
+                        if line=='}\n':
+                            line="".join(object_lines)
+                            object_lines=[]
+                        else:
+                            line=None
+
+                    if line:
+                        print("Detection Result: \n")
+                        print_json(json.loads(line))
+                except Exception as error:
+                    print(error)
+                
 def wait_for_pipeline(instance_id,
                       pipeline="object_detection",
                       version="1",
                       verbose=True):
     status = {"state":"RUNNING"}
-    while((status["state"]=="RUNNING") or (status["state"]==None)):
+    while((status["state"]=="RUNNING") or (status["state"]==None) or (status["state"]=="QUEUED")):
         status=get_status(instance_id,pipeline,version)
-        if (status==None):
+        if status==None:
             return None
-        if (verbose):
+        if verbose:
             print("Pipeline Status:\n")
             print_json(status)
         time.sleep(sleep_for_status)
@@ -100,14 +118,14 @@ def start_pipeline(stream_uri,
         pass
 
     request["destination"]["uri"] = urllib.parse.urljoin("file://",os.path.abspath(destination))
-    if (tags) and (len(tags) > 0):
+    if tags and len(tags) > 0:
         request["tags"] = tags
-    if (parameters) and (len(parameters) > 0):
+    if parameters and len(parameters) > 0:
         request["parameters"] = parameters
     pipeline_url = urllib.parse.urljoin(video_analytics_service,
                                         pipeline+"/"+version)
 
-    if (verbose):
+    if verbose:
         print("Starting Pipeline: %s" % (pipeline_url))
 
     try:
@@ -121,19 +139,20 @@ def start_pipeline(stream_uri,
 def print_stats(status,key='avg_fps'):
     values = [x[key] for x in status if x and key in x and 'state' in x and x['state']=="COMPLETED"]
 
-    if (len(values)):
+    if len(values):
         stats = {"value":key,
                  "Average":statistics.mean(values),
                  "Variance":statistics.variance(values),
                  "Max":max(values),
                  "Min":min(values),
-                 "Count":len(status),
+                 "Count":len(status)
         }
         print_json(stats)
     else:
         print("No results")
     
 if __name__ == "__main__":
+
     try:
         options, args = get_options()
     except Exception as error:
@@ -146,6 +165,6 @@ if __name__ == "__main__":
         status.append(wait_for_pipeline(instance_id,options.pipeline,verbose=options.verbose))
         read_detection_results(options.destination,verbose=options.verbose)
         
-    if (len(status)>1):
+    if len(status)>1:
         print_stats(status)
         print_stats(status,key="elapsed_time")
