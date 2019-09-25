eb74b5ff5564b351e06ed38d4444ccff5e92fe38
Pengfei Qu <pengfei.qu@intel.com>
2019-09-25 11:28:47 +0800

    add the script for VCAC-A setup and image upload

---
+++ script/setup-vcac-a.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# setup host docker swarm if not already
+docker swarm leave --force 2> /dev/null
+docker swarm init --advertise-addr=172.32.1.254 2> /dev/null || echo
+JOINCMD=$(docker swarm join-token worker | grep token)
+
+# setup VCAC-A passwordless access
+echo "Login to VACA-A once to establish passwordless access"
+if test ! -f ~/.ssh/id_rsa; then
+    cat /dev/zero | ssh-keygen -q -N ""
+    echo
+fi
+NODEUSER="root"
+NODEIP="172.32.1.1"
+ssh-copy-id $NODEUSER@$NODEIP 2> /dev/null || echo
+echo
+
+# setup node to join the host docker swarm 
+ssh $NODEUSER@$NODEIP "docker swarm leave --force 2> /dev/null;$JOINCMD"
+
+# setup node labels for office1
+for id in $(docker node ls -q 2> /dev/null); do
+    nodeip="$(docker node inspect -f {{.Status.Addr}} $id)"
+    if [[ -n "$(echo $nodeip | grep --fixed-strings $NODEIP)" ]]; then
+        echo "label $id: ad-insert_vcac_zone=yes"
+        docker node update --label-add ad-insert_vcac_zone=yes $id
+    fi
+done
---
+++ script/update-image.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -e
+
+function transfer_image {
+    image="$1"
+    worker="$2"
+
+    echo "Update image: $image to $worker"
+    sig1=$(docker image inspect -f {{.ID}} $image)
+    echo " local: $sig1"
+
+    sig2=$(ssh $worker "docker image inspect -f {{.ID}} $image 2> /dev/null || echo")
+    echo "remote: $sig2"
+
+    if test "$sig1" != "$sig2"; then
+        echo "Transfering image..."
+        docker save $image | ssh $worker "docker image rm -f $image 2>/dev/null; docker load"
+    fi
+    echo ""
+}
+
+DIR=$(dirname $(readlink -f "$0"))
+YML="${DIR}/../deployment/docker-swarm/docker-compose.yml"
+for id in $(docker node ls -q 2> /dev/null); do
+    ready="$(docker node inspect -f {{.Status.State}} $id)"
+    active="$(docker node inspect -f {{.Spec.Availability}} $id)"
+    nodeip="$(docker node inspect -f {{.Status.Addr}} $id)"
+    labels="$(docker node inspect -f {{.Spec.Labels}} $id)"
+    role="$(docker node inspect -f {{.Spec.Role}} $id)"
+
+    echo $ready $active $nodeip $labels $role
+    # skip unavailable or manager node
+    if test "$ready" = "ready"; then
+        if test "$active" = "active"; then
+            if test -z "$(hostname -I | grep --fixed-strings $nodeip)"; then
+
+                for image in $(awk -v constraints=1 -v role="node.role==${role}" -v labels="$labels" -f "$DIR/scan-yml.awk" "$YML"); do
+
+                    # trasnfer VCAC-A images to VCAC-A nodes only
+                    if [[ -n "$(echo $nodeip | grep --fixed-strings 172.32.1.1)" ]] || [[ "$(id -u)" -eq "0" ]]; then
+                        transfer_image $image "root@$nodeip"
+                    else
+                        transfer_image $image "$nodeip"
+                    fi
+
+                done
+
+            fi
+        fi
+    fi
+done
