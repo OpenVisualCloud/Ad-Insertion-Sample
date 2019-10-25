42cfcca6a9a5ecdcd8a21ea410e880c2577a770e
Pengfei Qu <pengfei.qu@intel.com>
2019-10-25 15:27:33 +0800

    update to the v19.10 for VCAC-A image

--- ad-insertion/video-analytics-service/update.sh
+++ ad-insertion/video-analytics-service/update.sh
@@ -4,7 +4,7 @@ DIR=$(dirname $(readlink -f "$0"))
 VCAC_A_DIR=$DIR/platforms/VCAC-A
 
 # gst base dockerfile vcac-a
-DOCKER_REPO="https://raw.githubusercontent.com/fkhoshne/Dockerfiles/vcaca/VCAC-A/ubuntu-16.04/analytics/gst"
+DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v19.10/VCAC-A/ubuntu-16.04/analytics/gst"
 
 base_name=vcac-a-ubuntu1604-openvino-gst-va
 docker_file="$VCAC_A_DIR/Dockerfile.2.gst.vcac-a"
@@ -13,7 +13,7 @@ echo "# "${base_name} > ${docker_file}
 curl ${DOCKER_REPO}/Dockerfile>> ${docker_file}
 
 # ffmpeg base dockerfile vcaa
-DOCKER_REPO="https://raw.githubusercontent.com/fkhoshne/Dockerfiles/vcaca/VCAC-A/ubuntu-16.04/analytics/ffmpeg"
+DOCKER_REPO="https://raw.githubusercontent.com/OpenVisualCloud/Dockerfiles/v19.10/VCAC-A/ubuntu-16.04/analytics/ffmpeg"
 
 base_name=vcac-a-ubuntu1604-openvino-ffmpeg-va
 docker_file="$VCAC_A_DIR/Dockerfile.4.ffmpeg.vcac-a"
