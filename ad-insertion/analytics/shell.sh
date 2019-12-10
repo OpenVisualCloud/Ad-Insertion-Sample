65581044639d944784ed3cc9c9fa1fdbab5b25a3
xwu2git <3871873+xwu2git@users.noreply.github.com>
2019-12-09 22:25:21 -0800

    build scripts

--- ad-insertion/analytics/shell.sh
+++ ad-insertion/analytics/shell.sh
@@ -1,22 +1,11 @@
 #!/bin/bash -e
 
 DIR=$(dirname $(readlink -f "$0"))
-case "$1" in 
-    ffmpeg)
-        IMAGE="video_analytics_service_ffmpeg"
-        DOCKERFILE="$DIR/Dockerfile.3"
-        ;;
-    gstreamer)
-        IMAGE="video_analytics_service_gstreamer"
-        DOCKERFILE="$DIR/Dockerfile.1"
-        ;;
-    *)
-        echo "Usage: ffmpeg|gstreamer"
-        exit 3
-        ;;
-esac
+PLATFORM=${1:-Xeon}
+shift
+FRAMEWORK=${1:-gst}
+shift
 
-shift    #first argument is parsed
-OPTIONS[0]="--rm"
-OPTIONS[1]="--name=${IMAGE}"
+IMAGE="ssai_analytics_${FRAMEWORK}_$(echo ${PLATFORM} | tr A-Z a-z)"
+OPTIONS=("--rm" "--name=${IMAGE}")
 . "$DIR/../../script/shell.sh"
