55b8fc1a440dfe4b9c2d8c0ad27c2910259b54fd
Pengfei Qu <pengfei.qu@intel.com>
2019-12-30 20:03:48 -0500

    ad static genearion with -DMINRESOLUTION

--- ad-insertion/ad-static/build.sh
+++ ad-insertion/ad-static/build.sh
@@ -11,12 +11,31 @@ FOD=1.0 # fade out duration
 SEG=5 # segment duration
 SEG_DASH=5000000 # segment duration
 FR=25
+MIN_H=360
 
 case "$(cat /proc/1/sched | head -n 1)" in
 *build.sh*)
     cd /mnt
+
+    if test "$1" == "adstatic"; then
+        if [ -n ${2} ]; then
+            MIN_H="$(echo ${2} |grep -oE '^[[:digit:]]+')"
+        fi
+    else
+        # $5 is the MINRESOLUTION if there are not args specified
+        if [ -n ${5} ]; then
+            MIN_H="$(echo ${5} |grep -oE '^[[:digit:]]+')"
+        fi
+    fi
+
     for s in "${SIZE[@]}"; do
         h=${s/*x}
+
+        if [ ${h} -lt ${MIN_H} ]; then
+          rm -f ${h}p*
+          continue
+        fi
+
         if test ! -f ${h}p_000.ts; then
             ffmpeg -f lavfi -i anullsrc=sample_rate=48000 -f lavfi -i color=c=blue:s=$s:d=$DE -vf "drawtext=text='Server-Side AD Insertion':x=(w-text_w)/2:y=(h-text_h)/2:fontsize=45:fontcolor_expr=ffffff%{eif\\\\: clip(255*(1*between(t\\, $DS + $FID\\, $DE - $FOD) + ((t - $DS)/$FID)*between(t\\, $DS\\, $DS + $FID) + (-(t - $DE)/$FOD)*between(t\\, $DE - $FOD\\, $DE) )\\, 0\\, 255) \\\\: x\\\\: 2 }" -shortest -c:a aac -c:v libx264 -g ${FR} -keyint_min ${FR} -f hls -hls_segment_filename ${h}p_%03d.ts -hls_time $SEG -y -hls_list_size $((DE/SEG)) ${h}p.m3u8
         fi
