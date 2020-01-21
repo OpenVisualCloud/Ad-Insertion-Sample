ae217240116fc050c41b46ebd8ae45d2bd44d2af
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-01-21 09:19:14 -0800

    move segment handling to its own threadpool

--- ad-insertion/frontend/segment.py
+++ ad-insertion/frontend/segment.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from tornado import web, gen
+from tornado.concurrent import run_on_executor
+from concurrent.futures import ThreadPoolExecutor
 from zkdata import ZKData
 from schedule import Schedule
 from os.path import isfile
@@ -15,6 +17,7 @@ class SegmentHandler(web.RequestHandler):
         super(SegmentHandler, self).__init__(app, request, **kwargs)
         self._sch=Schedule()
         self._usecase={"obj_detection":1, "emotion":0, "face_recognition":0}
+        self.executor=ThreadPoolExecutor()
 
     def check_origin(self, origin):
         return True
@@ -34,16 +37,11 @@ class SegmentHandler(web.RequestHandler):
         zk.set(zk_usecase_path,value)
         zk.close()
 
-    @gen.coroutine
-    def get(self):
-        stream = self.request.uri.replace("/segment/","")
+    @run_on_executor
+    def _get_segment(self, stream, user):
         stream_base = "/".join(stream.split("/")[:-1])
         print("stream: "+stream, flush=True)
         print("stream_base: "+stream_base, flush=True)
-        user = self.request.headers.get('X-USER')
-        if not user: 
-            self.set_status(400, "X-USER missing in headers")
-            return
 
         # Redirect if this is an AD stream.
         if stream.find("/adstream/") != -1:
@@ -57,59 +55,64 @@ class SegmentHandler(web.RequestHandler):
                             testfile=ad_storage_path+"/"+stream_base+"/"+m1.group(1)+".m3u8.complete"
                             print("Testing "+testfile, flush=True)
                             if isfile(testfile): 
-                                self.add_header('X-Accel-Redirect','/adinsert/'+stream)
-                                self.set_status(200,'OK')
-                                return
+                                return '/adinsert/'+stream
                     if stream.startswith("dash/"):
                         m1=re.search(".*/(.*)-(chunk|init).*",stream)
                         if m1:
                             testfile=ad_storage_path+"/"+stream_base+"/"+m1.group(1)+".mpd.complete"
                             print("Testing "+testfile, flush=True)
                             if isfile(testfile): 
-                                self.add_header('X-Accel-Redirect','/adinsert/'+stream)
-                                self.set_status(200,'OK')
-                                return
-                yield gen.sleep(0.5)
-
-            self.set_status(404, "AD not ready")
-            return
-
-        # Forward the media file request to content provider
-        print("Redirecting to /intercept/"+stream, flush=True)
-        self.set_header('X-Accel-Redirect','/intercept/' + stream)
+                                return '/adinsert/'+stream
+                time.sleep(0.5)
+            return None
 
         # get zk data for additional scheduling instruction
         zk=ZKData()
         seg_info=zk.get(zk_prefix+"/"+stream_base+"/"+user+"/"+stream.split("/")[-1])
         zk.close()
-        if not seg_info: return
+        if seg_info: 
+            # schedule ad
+            if "transcode" in seg_info:
+                self._sch.transcode(user, seg_info)
+                self._sch.flush()
 
-        # schedule ad
-        if "transcode" in seg_info:
-            self._sch.transcode(user, seg_info)
-            self._sch.flush()
+            # schedule analytics
+            if "analytics" in seg_info:
+                flag=0
+                for usecase in ["obj_detection", "emotion", "face_recognition"]:
+                    self._usecase[usecase]=self._get_usecase_status(user,usecase)
+                    flag += self._usecase[usecase]
+                if flag == 0:
+                    self._set_usecase_status(user,"obj_detection",1)
+                    self._usecase["obj_detection"]=1
 
-        # schedule analytics
-        if "analytics" in seg_info:
-            flag=0
-            for usecase in ["obj_detection", "emotion", "face_recognition"]:
-                self._usecase[usecase]=self._get_usecase_status(user,usecase)
-                flag += self._usecase[usecase]
-            if flag == 0:
-                self._set_usecase_status(user,"obj_detection",1)
-                self._usecase["obj_detection"]=1
+                if self._usecase["obj_detection"]==1:
+                    self._sch.analyze(seg_info, "object_detection")
+                if self._usecase["emotion"]==1:
+                    self._sch.analyze(seg_info, "emotion_recognition")
+                if self._usecase["face_recognition"] == 1:
+                    self._sch.analyze(seg_info, "face_recognition")
+                self._sch.flush()
 
-            if self._usecase["obj_detection"]==1:
-                self._sch.analyze(seg_info, "object_detection")
-            if self._usecase["emotion"]==1:
-                self._sch.analyze(seg_info, "emotion_recognition")
-            if self._usecase["face_recognition"] == 1:
-                self._sch.analyze(seg_info, "face_recognition")
-            self._sch.flush()
+                # delay releasing the stream to combat player caching.
+                if "seg_duration" in seg_info and seg_info["seg_time"]:
+                    time.sleep(max(0,seg_info["seg_duration"]-1.5))
+        return '/intercept/' + stream
 
-            # delay releasing the stream to combat player caching.
-            if "seg_duration" in seg_info and seg_info["seg_time"]:
-                yield gen.sleep(max(0,seg_info["seg_duration"]-1.5))
+    @gen.coroutine
+    def get(self):
+        stream = self.request.uri.replace("/segment/","")
+        user = self.request.headers.get('X-USER')
+        if not user: 
+            self.set_status(400, "X-USER missing in headers")
+            return
+
+        redirect=yield self._get_segment(stream, user)
+        if redirect is None:
+            self.set_status(404, "AD not ready")
+        else:
+            self.add_header('X-Accel-Redirect',redirect)
+            self.set_status(200,'OK')
 
     @gen.coroutine
     def post(self):
