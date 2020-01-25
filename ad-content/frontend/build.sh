03f91448b4b3af8b1ff6630c5030304c5649f926
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-01-24 18:13:53 -0800

    add obj and all inventory

--- ad-content/frontend/build.sh
+++ ad-content/frontend/build.sh
@@ -3,4 +3,11 @@
 IMAGE="ssai_ad_content_frontend"
 DIR=$(dirname $(readlink -f "$0"))
 
+case "$1" in 
+    Xeon)
+        cp -f inventory.json.all inventory.json;;
+    VCAC-A)
+        cp -f inventory.json.obj inventory.json;;
+esac
+
 . "$DIR/../../script/build.sh"
