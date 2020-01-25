87dee5fc3dc0ac0af42cafeb78061e789ff8f203
xwu2git <xxx@xxx>
2020-01-25 23:19:26 +0200

    separate ad-content-service image for Xeon and VCAC-A

--- ad-content/frontend/build.sh
+++ ad-content/frontend/build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
 
-IMAGE="ssai_ad_content_frontend"
+PLATFORM=$(echo $1 | tr A-Z a-z)
+IMAGE="ssai_ad_content_frontend_$PLATFORM"
 DIR=$(dirname $(readlink -f "$0"))
 
-case "$1" in 
-    Xeon)
-        cp -f "$DIR/inventory.json.all" "$DIR/inventory.json";;
-    VCAC-A)
-        cp -f "$DIR/inventory.json.obj" "$DIR/inventory.json";;
-esac
-
+cp -f "$DIR/inventory.json.$PLATFORM" "$DIR/inventory.json";;
 . "$DIR/../../script/build.sh"
