c0a2e6613f56268b9b3fb83578b8312aaf64e442
xwu2git <xxx@xxx>
2020-01-27 01:06:09 +0200

    restore to use per-session zk

--- common/zkdata.py
+++ common/zkdata.py
@@ -11,58 +11,28 @@ ZK_HOSTS='zookeeper-service:2181'
 class ZKData(object):
     def __init__(self):
         super(ZKData,self).__init__()
-        self._zk=None
-
-    def _connect(self):
-        if self._zk is None:
-            options={"max_tries":-1,"max_delay":5,"ignore_expire":True}
-            self._zk=KazooClient(hosts=ZK_HOSTS,connection_retry=options)
-            try:
-                self._zk.start(timeout=3*3600)
-            except:
-                print(traceback.format_exc(), flush=True)
-
-    def _set_set_cb(self, ao):
+        options={"max_tries":-1,"max_delay":5,"ignore_expire":True}
+        self._zk=KazooClient(hosts=ZK_HOSTS,connection_retry=options)
         try:
-            ao.get()
+            self._zk.start(timeout=3600)
         except:
             print(traceback.format_exc(), flush=True)
-            self._set_set(ao.arg_path,ao.arg_value)
-
-    def _set_set(self, path, value):
-        ao=self._zk.set_async(path,value)
-        ao.arg_path=path
-        ao.arg_value=value
-        ao.rawlink(self._set_set_cb)
-
-    def _set_create_cb(self, ao):
-        try:
-            ao.get()
-        except NodeExistsError:
-            self._zk.set_async(ao.arg_path,ao.arg_value)
-        except:
-            print(traceback.format_exc(), flush=True)
-            self.set(ao.arg_path, ao.arg_value)
 
     def set(self, path, value):
-        self._connect()
         value=json.dumps(value).encode('utf-8')
-        ao=self._zk.create_async(path,value,makepath=True)
-        ao.arg_path=path
-        ao.arg_value=value
-        ao.rawlink(self._set_create_cb)
+        try:
+            self._zk.create(path, value, makepath=True)
+        except NodeExistsError:
+            self._zk.set(path,value)
 
     def get(self, path):
-        self._connect()
         try:
-            value, stat= self._zk.retry(self._zk.get,path)
+            value, stat= self._zk.get(path)
             if not value: return {}
             return json.loads(value.decode('utf-8'))
         except Exception as e:
             return {}
 
     def close(self):
-        if self._zk:
-            self._zk.stop()
-            self._zk.close()
-            self._zk=None
+        self._zk.stop()
+        self._zk.close()
