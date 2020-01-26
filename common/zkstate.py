9f7615064c500eb437ee7110d4d9870bc31ffe14
xwu2git <xxx@xxx>
2020-01-26 22:52:02 +0200

    handle zk reconnect

--- common/zkstate.py
+++ common/zkstate.py
@@ -2,6 +2,7 @@
 
 from kazoo.client import KazooClient
 from kazoo.exceptions import NoNodeError, NodeExistsError
+from kazoo.protocol.states import KazooState
 import traceback
 import time
 
@@ -14,11 +15,13 @@ class ZKState(object):
 
     def set_path(self, path, name=None):
         if self._zk is None:
-            self._zk=KazooClient(hosts=ZK_HOSTS)
+            options={"max_tries":-1,"max_delay":5,"ignore_expire":True}
+            self._zk=KazooClient(hosts=ZK_HOSTS,connection_retry=options)
             try:
                 self._zk.start(timeout=3*3600)
             except:
                 print(traceback.format_exc(), flush=True)
+
         self._path=path
         self._name="" if name is None else name+"."
         self._zk.retry(self._zk.ensure_path,path)
@@ -47,3 +50,4 @@ class ZKState(object):
     def close(self):
         self._zk.stop()
         self._zk.close()
+        self._zk=None
