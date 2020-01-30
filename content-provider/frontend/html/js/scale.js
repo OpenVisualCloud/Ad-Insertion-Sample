0672ccfa4d46eaaaad8df31a94851493e222627b
xwu2git <3871873+xwu2git@users.noreply.github.com>
2020-01-29 17:52:59 -0800

    Revert "delay between launching multiple streams"
    This reverts commit 025330a679a4621a011e5647e9eeacda6573017d.

--- content-provider/frontend/html/js/scale.js
+++ content-provider/frontend/html/js/scale.js
@@ -36,50 +36,45 @@ function mklayout() {
     var signals=body.find("[signals]");
     signals.empty();
 
-    var delay=0;
     for (var r=0;r<nrows;r++) {
+        var y=(screen_height+margin*2)*r+header_height;
         for (var c=0;c<ncolumns;c++) {
-            setTimeout(function (r,c) {
-                var y=(screen_height+margin*2)*r+header_height;
-                var x=(screen_width+margin*2)*c;
-                if (c>=ncolumns/2) x=x+divide_width;
+            var x=(screen_width+margin*2)*c;
+            if (c>=ncolumns/2) x=x+divide_width;
+            var args={ 
+                seq: r*ncolumns+c, 
+                benchmark: spec("benchmark")?spec("benchmark"):"object",
+                header: "off",
+                playlist: "off",
+                videourl: "off",
+            };
+            var tmp=$('<div screen><iframe src="/?'+$.param(args)+'" scrolling="no" /><img src="image/screen.png"/></div>');
+            var view_port_width=screen_width1-margin*2;
+            var view_port_height=screen_height*0.8-margin*2;
+            tmp.find("iframe").attr("width",view_port_width).attr("height",view_port_height).css({width:view_port_width+"px",height:view_port_height+"px"});
+            tmp.find("img").attr("width",screen_width1).attr("height",screen_height);
 
-                var args={ 
-                    seq: r*ncolumns+c, 
-                    benchmark: spec("benchmark")?spec("benchmark"):"object",
-                    header: "off",
-                    playlist: "off",
-                    videourl: "off",
-                };
-                var tmp=$('<div screen><iframe src="/?'+$.param(args)+'" scrolling="no" /><img src="image/screen.png"/></div>');
-                var view_port_width=screen_width1-margin*2;
-                var view_port_height=screen_height*0.8-margin*2;
-                tmp.find("iframe").attr("width",view_port_width).attr("height",view_port_height).css({width:view_port_width+"px",height:view_port_height+"px"});
-                tmp.find("img").attr("width",screen_width1).attr("height",screen_height);
+            var view_port_x=(screen_width-screen_width1)/2;
+            tmp.css({width:screen_width1+"px",height:screen_height+"px",left:x+view_port_x+"px",top:y+"px"});
+            body.append(tmp);
 
-                var view_port_x=(screen_width-screen_width1)/2;
-                tmp.css({width:screen_width1+"px",height:screen_height+"px",left:x+view_port_x+"px",top:y+"px"});
-                body.append(tmp); 
+            var line=$(document.createElementNS(lines.attr('xmlns'),"line"));
+            line.attr({
+              x1: body_width/2,
+              x2: x+view_port_x+screen_width1/2,
+              y1: body_height/2,
+              y2: y+screen_height/2,
+              stroke: "white",
+              "stroke-width": 3,
+            });
+            lines.append(line);
 
-                var line=$(document.createElementNS(lines.attr('xmlns'),"line"));
-                line.attr({
-                    x1: body_width/2,
-                    x2: x+view_port_x+screen_width1/2,
-                    y1: body_height/2,
-                    y2: y+screen_height/2,
-                    stroke: "white",
-                    "stroke-width": 3,
-                });
-                lines.append(line);
-
-                var signal=$("#template [signal]").clone();
-                signal.css({
-                    "--signalx": x+view_port_x+screen_width1/2-body_width/2,
-                    "--signaly": y+screen_height/2-body_height/2,
-                });
-                signals.append(signal);
-            }, delay, r, c);
-            delay=delay+200;
+            var signal=$("#template [signal]").clone();
+            signal.css({
+              "--signalx": x+view_port_x+screen_width1/2-body_width/2,
+              "--signaly": y+screen_height/2-body_height/2,
+            });
+            signals.append(signal);
         }
     }
 }
